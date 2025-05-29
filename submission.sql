c46033191ba11e8e495840c86b6f8f523aa9506c
Timothee Thomance <timotheet11@gmail.com>
2025-05-29 16:12:09 -0400

    Workshop Submission

---
+++ submission.sql
@@ -0,0 +1,13 @@
+-- SELECT id, title, date, author FROM forum_posts WHERE content ILIKE '%emptystack%';
+-- SELECT * FROM forum_posts WHERE author = 'smart-money-44';
+-- SELECT * FROM forum_accounts WHERE username = 'smart-money-44';
+-- SELECT * FROM forum_accounts WHERE last_name = 'Steele';
+-- SELECT * FROM emptystack_accounts WHERE last_name = 'Steele';
+
+-- triple-cart-38 password456 Andrew Steele
+
+-- SELECT * FROM emptystack_messages WHERE body ILIKE '%taxi%';
+-- SELECT * FROM emptystack_accounts WHERE username = 'your-boss-99';
+-- SELECT * FROM emptystack_projects WHERE code = 'TAXI';
+
+-- notagaincarter Skylar Singer TAXI DczE0v2b
